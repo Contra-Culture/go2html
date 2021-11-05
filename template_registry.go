c2e1ec0e460d5778e236a53579669385a5176df4
Sinus Gorvalds <sinusgorvalds@twofacedjanus.com>
2021-11-05 13:37:22 +0300

    #55: reduces *TemplateRegistry & *TemplateRegistryDir types to TemplateReg

--- template_registry.go
+++ template_registry.go
@@ -6,99 +6,13 @@ import (
 	"strings"
 )
 
-type (
-	TemplateRegistry struct {
-		name string
-		dirs map[string]*TemplateRegistryDir
-	}
-	TemplateRegistryDir struct {
-		name     string
-		content  map[string]interface{}
-	}
-)
+type TemplatesReg map[string]interface{} // interface{} can contain values of *Template or TemplatesReg type
 
-func Reg(name string) *TemplateRegistry {
-	return &TemplateRegistry{
-		name: name,
-		dirs: map[string]*TemplateRegistryDir{},
-	}
-}
-func (r *TemplateRegistry) Mkdir(path []string) (*TemplateRegistryDir, error) {
-	if len(path) < 1 {
-		return nil, errors.New("wrong path, should not be empty")
-	}
-	chunk := path[0]
-	if len(chunk) < 1 {
-		return nil, errors.New("wrong path, path chunk should not be empty")
-	}
-	dir, exists := r.dirs[chunk]
-	if !exists {
-		dir = &TemplateRegistryDir{
-			name:     chunk,
-			content:  map[string]interface{}{},
-		}
-		r.dirs[chunk] = dir
-	}
-	if len(path) == 1 {
-		return dir, nil
-	}
-	return dir.Mkdir(path[1:])
-}
-func (r *TemplateRegistry) Dir(path []string) (*TemplateRegistryDir, error) {
-	if len(path) < 1 {
-		return nil, errors.New("wrong path, should not be empty")
-	}
-	chunk := path[0]
-	if len(chunk) < 1 {
-		return nil, errors.New("wrong path, path chunk should not be empty")
-	}
-	dir, exists := r.dirs[chunk]
-	if !exists {
-		return nil, fmt.Errorf("wrong path, dir \"%s\" is not found", chunk)
-	}
-	if len(path) == 1 {
-		return dir, nil
-	}
-	return dir.Dir(path[1:])
-}
-func (r *TemplateRegistry) Add(t *Template, path []string) (err error) {
-	if len(path) < 2 {
-		return fmt.Errorf("wrong path \"%s\", should have at least two chunks", strings.Join(path, "/"))
-	}
-	prevIdx := len(path) - 1
-	dir, err := r.Mkdir(path[:prevIdx])
-	if err != nil {
-		return
-	}
-	_, exists := dir.content[path[prevIdx]]
-	if exists {
-		return fmt.Errorf("template \"%s\" already exists", strings.Join(path, "/"))
-	}
-	dir.content[path[prevIdx]] = t
-	return
-}
-func (r *TemplateRegistry) Get(path []string) (t *Template, err error) {
-	if len(path) < 2 {
-		return nil, fmt.Errorf("wrong path \"%s\", should have at least two chunks", strings.Join(path, "/"))
-	}
-	templateKey := path[len(path)-1]
-	dirPath := path[:len(path)-1]
-	d, err := r.Dir(dirPath)
-	if err != nil {
-		return
-	}
-	i, exists := d.content[templateKey]
-	if !exists {
-		return nil, fmt.Errorf("template \"%s\" does not exist", strings.Join(path, "/"))
-	}
-	t, ok := i.(*Template)
-	if !ok {
-		return nil, fmt.Errorf("type error with \"%s\" path: found directory with \"%s\" name instead of template", strings.Join(path, "/"), path[len(path) - 1])
-	}
-	return
+func Reg() TemplatesReg {
+	return map[string]interface{}{}
 }
 
-func (d *TemplateRegistryDir) Mkdir(path []string) (*TemplateRegistryDir, error) {
+func (d TemplatesReg) Mkdir(path []string) (TemplatesReg, error) {
 	if len(path) < 1 {
 		return nil, errors.New("wrong path, should not be empty")
 	}
@@ -106,29 +20,26 @@ func (d *TemplateRegistryDir) Mkdir(path []string) (*TemplateRegistryDir, error)
 	if len(chunk) < 1 {
 		return nil, errors.New("wrong path, path chunk should not be empty")
 	}
-	i, exists := d.content[chunk]
+	i, exists := d[chunk]
 	if !exists {
-		dir := &TemplateRegistryDir{
-			name:     chunk,
-			content:  map[string]interface{}{},
-		}
-		d.content[chunk] = dir
-		if len(path) == 1 {
-			return dir, nil
-		}
-		return dir.Mkdir(path[1:])
-	} else {
-		dir, ok := i.(*TemplateRegistryDir)
-		if !ok {
-			return nil, fmt.Errorf("naming conflict with \"%s\" path: there is already a template with \"%s\" name", strings.Join(path, "/"), path[len(path) - 1])
-		}
+		dir := TemplatesReg{}
+		d[chunk] = dir
 		if len(path) == 1 {
 			return dir, nil
 		}
 		return dir.Mkdir(path[1:])
 	}
+	switch dir := i.(type) {
+	case TemplatesReg:
+		if len(path) == 1 {
+			return dir, nil
+		}
+		return dir.Mkdir(path[1:])
+	default:
+		return nil, fmt.Errorf("naming conflict with \"%s\" path: there is already a template with \"%s\" name", strings.Join(path, "/"), path[len(path) - 1])
+	}
 }
-func (d *TemplateRegistryDir) Dir(path []string) (*TemplateRegistryDir, error) {
+func (d TemplatesReg) Dir(path []string) (TemplatesReg, error) {
 	if len(path) < 1 {
 		return nil, errors.New("wrong path, should not be empty")
 	}
@@ -136,29 +47,30 @@ func (d *TemplateRegistryDir) Dir(path []string) (*TemplateRegistryDir, error) {
 	if len(chunk) < 1 {
 		return nil, errors.New("wrong path, path chunk should not be empty")
 	}
-	i, exists := d.content[chunk]
+	i, exists := d[chunk]
 	if !exists {
 		return nil, fmt.Errorf("wrong path, dir \"%s\" is not found", chunk)
 	}
-	dir, ok := i.(*TemplateRegistryDir)
-	if !ok {
+	switch dir := i.(type) {
+	case TemplatesReg:
+		if len(path) == 1 {
+			return dir, nil
+		}
+		return dir.Dir(path[1:])
+	default:
 		return nil, fmt.Errorf("type error with \"%s\" path: found template with \"%s\" name instead of directory", strings.Join(path, "/"), path[len(path) - 1])
 	}
-	if len(path) == 1 {
-		return dir, nil
-	}
-	return dir.Dir(path[1:])
 }
-func (d *TemplateRegistryDir) Add(t *Template, path []string) (err error) {
+func (d TemplatesReg) Add(t *Template, path []string) (err error) {
 	if len(path) < 1 {
 		return errors.New("wrong path, should have at least one chunk")
 	}
 	if len(path) == 1 {
-		_, exists := d.content[path[0]]
+		_, exists := d[path[0]]
 		if exists {
-			return fmt.Errorf("template \"%s\" already exists", path[0])
+			return fmt.Errorf("template \"%s\" already exists", strings.Join(path, "/"))
 		}
-		d.content[path[0]] = t
+		d[path[0]] = t
 		return
 	}
 	prevIdx := len(path) - 1
@@ -166,28 +78,28 @@ func (d *TemplateRegistryDir) Add(t *Template, path []string) (err error) {
 	if err != nil {
 		return
 	}
-	_, exists := dir.content[path[prevIdx]]
+	_, exists := dir[path[prevIdx]]
 	if exists {
-		// FIXME: Looks like this if condition is not correct.
-		return fmt.Errorf("template \"%s\" does not exist", strings.Join(path, "/"))
+		return fmt.Errorf("template \"%s\" already exists", strings.Join(path, "/"))
 	}
-	dir.content[path[prevIdx]] = t
+	dir[path[prevIdx]] = t
 	return
 }
-func (d *TemplateRegistryDir) Get(path []string) (t *Template, err error) {
+func (d TemplatesReg) Get(path []string) (t *Template, err error) {
 	if len(path) < 1 {
 		return nil, fmt.Errorf("wrong path \"%s\", should have at least one chunk", strings.Join(path, "/"))
 	}
 	if len(path) == 1 {
-		i, exists := d.content[path[0]]
+		i, exists := d[path[0]]
 		if !exists {
 			return nil, fmt.Errorf("template \"%s\" does not exist", strings.Join(path, "/"))
 		}
-		t, ok := i.(*Template)
-		if !ok {
+		switch t := i.(type) {
+		case *Template:
+			return t, nil
+		default:
 			return nil, fmt.Errorf("type error with \"%s\" path: found directory with \"%s\" name instead of template", strings.Join(path, "/"), path[len(path) - 1])
 		}
-		return t, nil
 	}
 	templateKey := path[len(path)-1]
 	dirPath := path[:len(path)-1]
@@ -195,13 +107,14 @@ func (d *TemplateRegistryDir) Get(path []string) (t *Template, err error) {
 	if err != nil {
 		return
 	}
-	i, exists := dir.content[templateKey]
+	i, exists := dir[templateKey]
 	if !exists {
 		return nil, fmt.Errorf("template \"%s\" does not exist", strings.Join(path, "/"))
 	}
-	t, ok := i.(*Template)
-	if !ok {
+	switch t := i.(type) {
+	case *Template:
+		return t, nil
+	default:
 		return nil, fmt.Errorf("type error with \"%s\" path: found directory with \"%s\" name instead of template", strings.Join(path, "/"), path[len(path) - 1])
 	}
-	return
 }
--- template_registry_test.go
+++ template_registry_test.go
@@ -7,17 +7,11 @@ import (
 )
 
 var _ = Describe("template registry", func() {
-	Describe("TemplateRegistry", func() {
-		Describe("Reg()", func() {
-			It("returns registry", func() {
-				r := Reg("test")
-				Expect(r).NotTo(BeNil())
-			})
-		})
+	Describe("TemplatesReg", func() {
 		Describe(".Mkdir()", func() {
 			Context("when does not exist", func() {
 				It("returns dir", func() {
-					r := Reg("test")
+					r := Reg()
 					_, err := r.Mkdir([]string{"1"})
 					Expect(err).To(BeNil())
 					_, err = r.Mkdir([]string{"1", "1-1"})
@@ -31,7 +25,7 @@ var _ = Describe("template registry", func() {
 			})
 			Context("when exists", func() {
 				It("returns dir", func() {
-					r := Reg("test")
+					r := Reg()
 					_, err := r.Mkdir([]string{"1"})
 					Expect(err).To(BeNil())
 					dir, err := r.Mkdir([]string{"1"})
@@ -58,7 +52,7 @@ var _ = Describe("template registry", func() {
 		Describe(".Dir()", func() {
 			Context("when exists", func() {
 				It("returns dir", func() {
-					r := Reg("test")
+					r := Reg()
 					r.Mkdir([]string{"1"})
 					r.Mkdir([]string{"1", "1-1"})
 					r.Mkdir([]string{"1", "1-2"})
@@ -79,7 +73,7 @@ var _ = Describe("template registry", func() {
 			})
 			Context("when does not exist", func() {
 				It("fails and returns error", func() {
-					r := Reg("test")
+					r := Reg()
 					r.Mkdir([]string{"1"})
 					r.Mkdir([]string{"1", "1-1"})
 					dir, err := r.Dir([]string{"2"})
@@ -96,7 +90,7 @@ var _ = Describe("template registry", func() {
 		Describe(".Add()", func() {
 			Context("when exists", func() {
 				It("fails and returns error", func() {
-					r := Reg("test")
+					r := Reg()
 					r.Mkdir([]string{"1", "1-1", "1-1-1"})
 					err := r.Add(&Template{}, []string{"1", "1-1", "1-1-1", "test-template"})
 					Expect(err).NotTo(HaveOccurred())
@@ -107,7 +101,7 @@ var _ = Describe("template registry", func() {
 			})
 			Context("when does not exist", func() {
 				It("adds template", func() {
-					r := Reg("test")
+					r := Reg()
 					r.Mkdir([]string{"1", "1-1", "1-1-1"})
 					err := r.Add(&Template{}, []string{"1", "1-1", "1-1-1", "test-template"})
 					Expect(err).NotTo(HaveOccurred())
@@ -117,7 +111,7 @@ var _ = Describe("template registry", func() {
 		Describe(".Get()", func() {
 			Context("when exists", func() {
 				It("returns template", func() {
-					r := Reg("test")
+					r := Reg()
 					r.Mkdir([]string{"1", "1-1", "1-1-1"})
 					err := r.Add(&Template{}, []string{"1", "1-1", "1-1-1", "test-template"})
 					Expect(err).NotTo(HaveOccurred())
@@ -128,126 +122,12 @@ var _ = Describe("template registry", func() {
 			})
 			Context("when does not exist", func() {
 				It("fails and returns error", func() {
-					r := Reg("test")
+					r := Reg()
 					r.Mkdir([]string{"1", "1-1", "1-1-1"})
 					t, err := r.Get([]string{"1", "1-1", "1-1-1", "test-template"})
 					Expect(err).To(HaveOccurred())
 					Expect(err.Error()).To(Equal("template \"1/1-1/1-1-1/test-template\" does not exist"))
 					Expect(t).To(BeNil())
-				})
-			})
-		})
-		Describe("TemplateRegistryDir", func() {
-			Describe(".Mkdir()", func() {
-				Context("when does not exist", func() {
-					It("returns dir", func() {
-						r := Reg("test")
-						dir, err := r.Mkdir([]string{"1"})
-						Expect(err).To(BeNil())
-						dir, err = dir.Mkdir([]string{"1", "1-1"})
-						Expect(err).NotTo(HaveOccurred())
-						dir, err = dir.Mkdir([]string{"2", "2-1", "2-1-1"})
-						Expect(err).NotTo(HaveOccurred())
-						dir, err = dir.Mkdir([]string{"2", "2-1", "2-1-2"})
-						Expect(err).NotTo(HaveOccurred())
-						Expect(dir).NotTo(BeNil())
-					})
-				})
-				Context("when exists", func() {
-					It("returns dir", func() {
-						r := Reg("test")
-						dir1, err := r.Mkdir([]string{"1"})
-						Expect(err).To(BeNil())
-						_, err = dir1.Mkdir([]string{"1-1"})
-						Expect(err).NotTo(HaveOccurred())
-						dir1_1, err := dir1.Mkdir([]string{"1-1"})
-						Expect(err).NotTo(HaveOccurred())
-						Expect(dir1_1).NotTo(BeNil())
-						_, err = dir1.Mkdir([]string{"1-2"})
-						Expect(err).To(BeNil())
-						dir1_2, err := dir1.Mkdir([]string{"1-2"})
-						Expect(err).NotTo(HaveOccurred())
-						Expect(dir1_2).NotTo(BeNil())
-					})
-				})
-			})
-			Describe(".Dir()", func() {
-				Context("when exists", func() {
-					It("returns dir", func() {
-						r := Reg("test")
-						r.Mkdir([]string{"1"})
-						r.Mkdir([]string{"1", "1-1"})
-						r.Mkdir([]string{"1", "1-2"})
-						r.Mkdir([]string{"1", "1-1", "1-1-1"})
-						dir, err := r.Dir([]string{"1"})
-						Expect(err).NotTo(HaveOccurred())
-						_, err = dir.Dir([]string{"1-1"})
-						Expect(err).NotTo(HaveOccurred())
-						_, err = dir.Dir([]string{"1-2"})
-						Expect(err).NotTo(HaveOccurred())
-						dir1_1_1, err := dir.Dir([]string{"1-1", "1-1-1"})
-						Expect(err).NotTo(HaveOccurred())
-						Expect(dir1_1_1).NotTo(BeNil())
-					})
-				})
-				Context("when does not exist", func() {
-					It("fails and returns error", func() {
-						r := Reg("test")
-						dir, _ := r.Mkdir([]string{"1"})
-						r.Mkdir([]string{"1", "1-1"})
-						dir1_2, err := dir.Dir([]string{"2"})
-						Expect(err).To(HaveOccurred())
-						Expect(err.Error()).To(Equal("wrong path, dir \"2\" is not found"))
-						Expect(dir1_2).To(BeNil())
-						dir1_1_1, err := dir.Dir([]string{"1-1", "1-1-1"})
-						Expect(err).To(HaveOccurred())
-						Expect(err.Error()).To(Equal("wrong path, dir \"1-1-1\" is not found"))
-						Expect(dir1_1_1).To(BeNil())
-					})
-				})
-			})
-			Describe(".Add()", func() {
-				Context("when exists", func() {
-					It("fails and returns error", func() {
-						r := Reg("test")
-						dir, _ := r.Mkdir([]string{"1", "1-1", "1-1-1"})
-						err := dir.Add(&Template{}, []string{"test-template"})
-						Expect(err).NotTo(HaveOccurred())
-						err = dir.Add(&Template{}, []string{"test-template"})
-						Expect(err).To(HaveOccurred())
-						Expect(err.Error()).To(Equal("template \"test-template\" already exists"))
-					})
-				})
-				Context("when does not exist", func() {
-					It("adds template", func() {
-						r := Reg("test")
-						dir, _ := r.Mkdir([]string{"1", "1-1", "1-1-1"})
-						err := dir.Add(&Template{}, []string{"test-template"})
-						Expect(err).NotTo(HaveOccurred())
-					})
-				})
-			})
-			Describe(".Get()", func() {
-				Context("when exists", func() {
-					It("returns template", func() {
-						r := Reg("test")
-						dir, _ := r.Mkdir([]string{"1", "1-1", "1-1-1"})
-						err := r.Add(&Template{}, []string{"1", "1-1", "1-1-1", "test-template"})
-						Expect(err).NotTo(HaveOccurred())
-						t, err := dir.Get([]string{"test-template"})
-						Expect(err).NotTo(HaveOccurred())
-						Expect(t).NotTo(BeNil())
-					})
-				})
-				Context("when does not exist", func() {
-					It("fails and returns error", func() {
-						r := Reg("test")
-						dir, _ := r.Mkdir([]string{"1", "1-1", "1-1-1"})
-						t, err := dir.Get([]string{"test-template"})
-						Expect(err).To(HaveOccurred())
-						Expect(err.Error()).To(Equal("template \"test-template\" does not exist"))
-						Expect(t).To(BeNil())
-					})
 				})
 			})
 		})
